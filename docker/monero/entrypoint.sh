7dcb80217efa800ad45f970d6c8532b1660d0ba6
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2021-09-13 17:49:39 +0200

    Add ban-list to monero

--- docker/monero/entrypoint.sh
+++ docker/monero/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if curl --fail --connect-timeout 10 --verbose https://gui.xmr.pm/files/block.txt -O /tmp/block.txt; then
+  mv /tmp/block.txt /var/monero/block.txt
+fi
+
 exec "/monerod" \
 --data-dir /var/monero \
+--ban-list /var/monero/block.txt \
 --zmq-pub=tcp://0.0.0.0:18083 --rpc-bind-ip=0.0.0.0 --rpc-bind-port 18081 \
 --non-interactive --confirm-external-bind --restricted-rpc --allow-local-ip --no-igd \
 --fast-block-sync 1 --prep-blocks-threads $(nproc) --sync-pruned-blocks --prune-blockchain --check-updates disabled \
