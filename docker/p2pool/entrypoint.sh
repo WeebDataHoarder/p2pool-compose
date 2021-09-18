62d31c5da76abe4883fc0ef719a096667f9eff5f
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2021-09-19 01:38:04 +0200

    Remove config.json override

--- docker/p2pool/entrypoint.sh
+++ docker/p2pool/entrypoint.sh
@@ -5,16 +5,9 @@ while [[ "$(curl --silent http://monero:18081/json_rpc -d '{"jsonrpc":"2.0","id"
   sleep 1
 done
 
-P2POOL_CONFIG=""
-
-if [[ -f "/p2pool/data/config.json" ]]; then
-  P2POOL_CONFIG="--config /p2pool/data/config.json"
-fi
-
 echo "Starting p2pool with config "
 
 exec "/p2pool/p2pool" \
-${P2POOL_CONFIG} \
 --host monero \
 --rpc-port 18081 --zmq-port 18083 \
 "$@"
